254a77464f9693c9eb23a9a3b0019268ac8690e6
George <epifantsev.george@gmail.com>
2023-03-27 14:33:42 +0700

    Lab3 done task 2-3

--- lab3/src/index.js
+++ lab3/src/index.js
@@ -1,10 +1,10 @@
-import _ from 'lodash';
-
 import Swiper, { Navigation, Pagination } from 'swiper';
 // import Swiper and modules styles
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
+
+Swiper.use([Navigation,Pagination]);
 
 const swiper = new Swiper('.mySwiper', {
     loop: true,
