4a550ab02895ffe5f2d9d77f0032a478528bfae4
George <epifantsev.george@gmail.com>
2023-04-04 22:27:18 +0700

    Fix lab3 task 3

--- lab3/task1-3/src/index.js
+++ lab3/task1-3/src/index.js
@@ -3,6 +3,7 @@ import Swiper, { Navigation, Pagination } from 'swiper';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
+import './index.css'
 
 Swiper.use([Navigation,Pagination]);
 
