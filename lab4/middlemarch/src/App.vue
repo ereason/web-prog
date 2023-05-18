a8d47454e164262e811eb438b114dbc59d9fd526
George <george@Georges-MacBook-Air.local>
2023-05-18 23:08:19 +0700

    lab4 done

---
+++ lab4/middlemarch/src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+
+</script>
+
+<template>
+  <router-view></router-view>
+</template>
+
+<style>
+html, body {
+  background-color: #7678ed!important;
+}
+
+#app {
+  font-family: Montserrat, Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
---
+++ lab4/middlemarch/src/components/Footer.vue
@@ -0,0 +1,58 @@
+<template>
+  <footer>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="footer-content">
+          <div class="logo-home"></div>
+          <div class="footer-text">
+            <div class="social-links">
+              <span class="social vk"><a href="https://vk.com/epifantsev_george" target="_blank">VK</a></span>
+              <span class="social github"><a href="https://github.com/ereason/web-prog" target="_blank">Github</a></span>
+            </div>
+
+            <p>2023 НГТУ АВТФ АП-026 Епифанцев Георгий.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<style scoped>
+footer {
+  min-height: 100px;
+  text-align: center;
+    background-color: rgba(243, 91, 4, 0.8);
+
+  font-size: 16px;
+  font-weight: 200;
+  color: #e3dec6;
+  padding-right: 20px;
+  padding-bottom: 20px;
+
+    border-radius: 25px;
+
+    margin-top: 15px;
+    margin-left: 25px;
+    margin-right: 25px;
+
+    filter: drop-shadow(0 0 0.75rem rgba(194, 148, 14, 0.8));
+}
+
+a{
+    color: #c6d7e3;
+}
+</style>
+
+<script>
+export default {
+  data() {
+    return {
+
+    };
+  },
+  methods: {
+
+  }
+};
+</script>
