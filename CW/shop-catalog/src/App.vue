5847b597d7adaedd9e8b60f22befbbcddf697857
George <george@Georges-MacBook-Air.local>
2023-05-18 23:09:33 +0700

    start CW

---
+++ CW/shop-catalog/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <Catalog/>
+</template>
+
+<script setup>
+import Catalog from "@/components/Catalog.vue";
+</script>
+
+<style scoped>
+
+
+
+@media (min-width: 1024px) {
+
+}
+</style>
